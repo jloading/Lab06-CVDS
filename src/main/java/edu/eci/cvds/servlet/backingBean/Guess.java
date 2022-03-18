f406833cc137ee8dcb757d12a88242915dfda875
Juanca <juan.acosta-lo@mail.escuelaing.edu.co>
2022-03-18 00:03:39 -0500

    Creación guess.xhtml

--- src/main/java/edu/eci/cvds/servlet/backingBean/Guess.java
+++ src/main/java/edu/eci/cvds/servlet/backingBean/Guess.java
@@ -1,12 +1,24 @@
 package edu.eci.cvds.servlet.backingBean;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.Random;
+
+@ManagedBean(name = "guessBean")
+@ApplicationScoped
+
 public class Guess {
     private int numeroAdivinar;
     private int numeroIntentos;
     private int premioAcumulado;
     private String estado;
+    private int intentos;
 
     public Guess() {
+        setNumeroAdivinar();
+
     }
 
     public int getNumeroAdivinar() {
@@ -25,8 +37,10 @@ public class Guess {
         return estado;
     }
 
-    public void setNumeroAdivinar(int numeroAdivinar) {
-        this.numeroAdivinar = numeroAdivinar;
+    public void setNumeroAdivinar() {
+        Random random = new Random();
+        int x = random.nextInt(10);
+        this.numeroAdivinar = x;
     }
 
     public void setNumeroIntentos(int numeroIntentos) {
@@ -39,6 +53,24 @@ public class Guess {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    public void guess(int intento){
+        intentos += 1;
+        if(intento == numeroAdivinar){
+            setEstado("Adivinó");
+        }
+        else{
+            premioAcumulado -= 10000;
+            setEstado("No adivinó");
+        }
+    }
+
+    public void restart(){
+        premioAcumulado = 100000;
+        intentos = 0;
+        setNumeroAdivinar();
+        setEstado("No adivinó");
     }
 
 
