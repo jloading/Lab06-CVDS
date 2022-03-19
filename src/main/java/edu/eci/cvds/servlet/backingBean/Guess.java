4353f48af0bc6813e0bcfcb5cc1eff1d2f961f0e
Juanca <juan.acosta-lo@mail.escuelaing.edu.co>
2022-03-18 22:48:46 -0500

    vuelta a front y back inicial

---
+++ src/main/java/edu/eci/cvds/servlet/backingBean/Guess.java
@@ -0,0 +1,77 @@
+package edu.eci.cvds.servlet.backingBean;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.Random;
+
+@ManagedBean(name = "guessBean")
+@ApplicationScoped
+
+public class Guess {
+    private int numeroAdivinar;
+    private int numeroIntentos = 0;
+    private int premioAcumulado = 100000;
+    private String estado;
+
+    public Guess() {
+    }
+
+    public int getNumeroAdivinar() {
+        return numeroAdivinar;
+    }
+
+    public int getNumeroIntentos() {
+        return numeroIntentos;
+    }
+
+    public int getPremioAcumulado() {
+        return premioAcumulado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setNumeroAdivinar() {
+        Random random = new Random();
+        int x = random.nextInt(10);
+        this.numeroAdivinar = x;
+    }
+
+    public void setNumeroIntentos(int numeroIntentos) {
+        this.numeroIntentos = numeroIntentos;
+    }
+
+    public void setPremioAcumulado(int premioAcumulado) {
+        this.premioAcumulado = premioAcumulado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void guess(int intento){
+
+        numeroIntentos += 1;
+        if(intento == numeroAdivinar){
+            setEstado("Adivinó");
+        }
+        else{
+            premioAcumulado -= 10000;
+            setEstado("No adivinó");
+        }
+    }
+
+    public void restart(){
+        premioAcumulado = 100000;
+        numeroIntentos = 0;
+        setNumeroAdivinar();
+        setEstado("No adivinó");
+    }
+
+
+
+
+}
